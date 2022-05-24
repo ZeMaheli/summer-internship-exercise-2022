33fb64e0bdf48df07fd7b63666c6d532df8b2f72
48249 - José Viegas <josemcv99@hotmail.com>
2022-05-24 21:38:31 +0100

    finished with all tests

--- src/main/java/com/premiumminds/internship/screenlocking/AllPatterns.java
+++ src/main/java/com/premiumminds/internship/screenlocking/AllPatterns.java
@@ -1,80 +1,62 @@
 package com.premiumminds.internship.screenlocking;
 
+import java.util.concurrent.Callable;
 
+public class AllPatterns implements Callable<Integer> {
 
+    private int max;
+    private int length;
 
-public class AllPatterns implements Callable<Integer>{
+    public AllPatterns(int m, int n){
+        max = m;
+        length = n;
+    }
 
-    private Integer[][] pattern = {{1,2,3},{4,5,6},{7,8,9}};
-    private Integer size = Arrays.asList(pattern).size();
-
-    GraphStruct graphPattern = new GraphStruct();
-
-    public Integer startingPoint;
-    public Integer length;
-
-    public AllPatterns(Integer startPoint, Integer startLength) {
-        startingPoint = startPoint;
-        length = startLength;
+    public static int numberOfPatterns(int start, int length) {
+        // Keep a recod of invalid points on the path between two selected points 
+        int skip[][] = new int[10][10];
+        //In order to point 1 connect to point 3 and vice versa, point 2 must be visited
+        skip[1][3] = skip[3][1] = 2;
+        //In order to point 1 connect to point 7 and vice versa, point 4 must be visited
+        skip[1][7] = skip[7][1] = 4;
+        //In order to point 3 connect to point 9 and vice versa, point 6 must be visited
+        skip[3][9] = skip[9][3] = 6;
+        //In order to point 7 connect to point 9 and vice versa, point 8 must be visited
+        skip[7][9] = skip[9][7] = 8;
+        //Point 5 must be visited so these points can connetc to each other
+        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5;
+        boolean visited[] = new boolean[10];
+        return DFS(visited, skip, start, length);
     }
     
-    private Integer pointToPatternFirstIndex(Integer point){
-        return point >> 2;
-    }
-    
-    private Integer pointToPatternSecondIndex(Integer point){
-        Integer index = point%size;
-        if(index == 0) return 2; 
-        else return index-1;
-    }
-
-    private Boolean isMiddleVisited(Integer point){
-        GraphStruct.Vertex[] adj = graphPattern.adjTable.get(graphPattern.integerToVertex(point));
-        Vertex firstVertex;
-        Vertex secondVertex;
-        switch(point){
-            case 1:
-                firstVertex = graphPattern.integerToVertex(point+1);
-                secondVertex = graphPattern.integerToVertex(point+size);
-                break;
-            case 3:
-                firstVertex = graphPattern.integerToVertex(point-1);
-                secondVertex = graphPattern.integerToVertex(point+size);
-            break;
-            case 7:
-                firstVertex = graphPattern.integerToVertex(point-size);
-                secondVertex = graphPattern.integerToVertex(point+1);
-            break;
-            case 9:
-                firstVertex = graphPattern.integerToVertex(point-1);
-                secondVertex = graphPattern.integerToVertex(point-size);
-            break;
-            ArrayUtils.contains(adj,firstVertex);
+    private static int DFS(boolean[] visited, int[][] skip, int cur, int remain) {
+        // Base case: out of bounds
+        if (remain < 1) return 0;
+        // max legnth reached
+        if (remain == 1) return 1;
+        // Mark current point as visited
+        visited[cur] = true;
+        int res = 0;
+  
+        for (int i = 1; i <= 9; i++) {
+            // Next point must be unvisited
+            // There is no point between current and next or skip point is already visited
+            if (!visited[i] && (skip[cur][i] == 0 || visited[skip[cur][i]])) {
+                res += DFS(visited, skip, i, remain-1);
+            }
         }
-        return 
+        // Mark the current one unvisited so we can continue to search other paths  
+        visited[cur] = false;
+        return res;
     }
 
-
-    public Integer getOppositePoint(Integer point){
-        
+    public static void main(String[] args) {
+        System.out.println(numberOfPatterns(5,2));
     }
 
-    private Integer pointFromPattern(Integer firstIndex, Integer secondIndex){
-        try{
-          Integer p = pattern[firstIndex][secondIndex];
-          return p;
-        } catch(Exception e){
-          return null;
-        }
-    }
-    
-    public Integer getPatterns(){
-
-    }
-    
     @Override
     public Integer call() throws Exception {
-        return getPatterns();
+        return numberOfPatterns(max, length);
     }
+
 }
-    
--- src/test/java/com/premiumminds/internship/screenlocking/ScreenLockinPatternTest.java
+++ src/test/java/com/premiumminds/internship/screenlocking/ScreenLockinPatternTest.java
@@ -2,7 +2,6 @@ package com.premiumminds.internship.screenlocking;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -18,15 +17,174 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class ScreenLockinPatternTest {
 
-  /**
-   * The corresponding implementations to test.
-   *
-   * If you want, you can make others :)
-   *
-   */
-  public ScreenLockinPatternTest() {
-  };
+  @Test
+  public void ScreenLockinPatternTestLength9()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count1  = new ScreenLockinPattern().countPatternsFrom(1, 8);
+    Future<Integer> count2 = new ScreenLockinPattern().countPatternsFrom(2, 8);
+    Future<Integer> count3  = new ScreenLockinPattern().countPatternsFrom(3, 8);
+    Future<Integer> count4  = new ScreenLockinPattern().countPatternsFrom(4, 8);
+    Future<Integer> count5  = new ScreenLockinPattern().countPatternsFrom(5, 8);
+    Future<Integer> count6  = new ScreenLockinPattern().countPatternsFrom(6, 8);
+    Future<Integer> count7  = new ScreenLockinPattern().countPatternsFrom(7, 8);
+    Future<Integer> count8  = new ScreenLockinPattern().countPatternsFrom(8, 8);
+    Future<Integer> count9  = new ScreenLockinPattern().countPatternsFrom(9, 8);
+    Integer result = count1.get(10, TimeUnit.SECONDS)+count2.get(10, TimeUnit.SECONDS)
+    +count3.get(10, TimeUnit.SECONDS)+count4.get(10, TimeUnit.SECONDS)+count5.get(10, TimeUnit.SECONDS)
+    +count6.get(10, TimeUnit.SECONDS)+count7.get(10, TimeUnit.SECONDS)+count8.get(10, TimeUnit.SECONDS)+count9.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 140704);
+  }
 
+  @Test
+  public void ScreenLockinPatternTestLength8()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count1  = new ScreenLockinPattern().countPatternsFrom(1, 8);
+    Future<Integer> count2 = new ScreenLockinPattern().countPatternsFrom(2, 8);
+    Future<Integer> count3  = new ScreenLockinPattern().countPatternsFrom(3, 8);
+    Future<Integer> count4  = new ScreenLockinPattern().countPatternsFrom(4, 8);
+    Future<Integer> count5  = new ScreenLockinPattern().countPatternsFrom(5, 8);
+    Future<Integer> count6  = new ScreenLockinPattern().countPatternsFrom(6, 8);
+    Future<Integer> count7  = new ScreenLockinPattern().countPatternsFrom(7, 8);
+    Future<Integer> count8  = new ScreenLockinPattern().countPatternsFrom(8, 8);
+    Future<Integer> count9  = new ScreenLockinPattern().countPatternsFrom(9, 8);
+    Integer result = count1.get(10, TimeUnit.SECONDS)+count2.get(10, TimeUnit.SECONDS)
+    +count3.get(10, TimeUnit.SECONDS)+count4.get(10, TimeUnit.SECONDS)+count5.get(10, TimeUnit.SECONDS)
+    +count6.get(10, TimeUnit.SECONDS)+count7.get(10, TimeUnit.SECONDS)+count8.get(10, TimeUnit.SECONDS)+count9.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 140704);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestLength7()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count1  = new ScreenLockinPattern().countPatternsFrom(1, 7);
+    Future<Integer> count2 = new ScreenLockinPattern().countPatternsFrom(2, 7);
+    Future<Integer> count3  = new ScreenLockinPattern().countPatternsFrom(3, 7);
+    Future<Integer> count4  = new ScreenLockinPattern().countPatternsFrom(4, 7);
+    Future<Integer> count5  = new ScreenLockinPattern().countPatternsFrom(5, 7);
+    Future<Integer> count6  = new ScreenLockinPattern().countPatternsFrom(6, 7);
+    Future<Integer> count7  = new ScreenLockinPattern().countPatternsFrom(7, 7);
+    Future<Integer> count8  = new ScreenLockinPattern().countPatternsFrom(8, 7);
+    Future<Integer> count9  = new ScreenLockinPattern().countPatternsFrom(9, 7);
+    Integer result = count1.get(10, TimeUnit.SECONDS)+count2.get(10, TimeUnit.SECONDS)
+    +count3.get(10, TimeUnit.SECONDS)+count4.get(10, TimeUnit.SECONDS)+count5.get(10, TimeUnit.SECONDS)
+    +count6.get(10, TimeUnit.SECONDS)+count7.get(10, TimeUnit.SECONDS)+count8.get(10, TimeUnit.SECONDS)+count9.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 72912);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestLength6()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count1  = new ScreenLockinPattern().countPatternsFrom(1, 6);
+    Future<Integer> count2 = new ScreenLockinPattern().countPatternsFrom(2, 6);
+    Future<Integer> count3  = new ScreenLockinPattern().countPatternsFrom(3, 6);
+    Future<Integer> count4  = new ScreenLockinPattern().countPatternsFrom(4, 6);
+    Future<Integer> count5  = new ScreenLockinPattern().countPatternsFrom(5, 6);
+    Future<Integer> count6  = new ScreenLockinPattern().countPatternsFrom(6, 6);
+    Future<Integer> count7  = new ScreenLockinPattern().countPatternsFrom(7, 6);
+    Future<Integer> count8  = new ScreenLockinPattern().countPatternsFrom(8, 6);
+    Future<Integer> count9  = new ScreenLockinPattern().countPatternsFrom(9, 6);
+    Integer result = count1.get(10, TimeUnit.SECONDS)+count2.get(10, TimeUnit.SECONDS)
+    +count3.get(10, TimeUnit.SECONDS)+count4.get(10, TimeUnit.SECONDS)+count5.get(10, TimeUnit.SECONDS)
+    +count6.get(10, TimeUnit.SECONDS)+count7.get(10, TimeUnit.SECONDS)+count8.get(10, TimeUnit.SECONDS)+count9.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 26016);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestLength5()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count1  = new ScreenLockinPattern().countPatternsFrom(1, 5);
+    Future<Integer> count2 = new ScreenLockinPattern().countPatternsFrom(2, 5);
+    Future<Integer> count3  = new ScreenLockinPattern().countPatternsFrom(3, 5);
+    Future<Integer> count4  = new ScreenLockinPattern().countPatternsFrom(4, 5);
+    Future<Integer> count5  = new ScreenLockinPattern().countPatternsFrom(5, 5);
+    Future<Integer> count6  = new ScreenLockinPattern().countPatternsFrom(6, 5);
+    Future<Integer> count7  = new ScreenLockinPattern().countPatternsFrom(7, 5);
+    Future<Integer> count8  = new ScreenLockinPattern().countPatternsFrom(8, 5);
+    Future<Integer> count9  = new ScreenLockinPattern().countPatternsFrom(9, 5);
+    Integer result = count1.get(10, TimeUnit.SECONDS)+count2.get(10, TimeUnit.SECONDS)
+    +count3.get(10, TimeUnit.SECONDS)+count4.get(10, TimeUnit.SECONDS)+count5.get(10, TimeUnit.SECONDS)
+    +count6.get(10, TimeUnit.SECONDS)+count7.get(10, TimeUnit.SECONDS)+count8.get(10, TimeUnit.SECONDS)+count9.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 7152);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestLength4()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count1  = new ScreenLockinPattern().countPatternsFrom(1, 4);
+    Future<Integer> count2 = new ScreenLockinPattern().countPatternsFrom(2, 4);
+    Future<Integer> count3  = new ScreenLockinPattern().countPatternsFrom(3, 4);
+    Future<Integer> count4  = new ScreenLockinPattern().countPatternsFrom(4, 4);
+    Future<Integer> count5  = new ScreenLockinPattern().countPatternsFrom(5, 4);
+    Future<Integer> count6  = new ScreenLockinPattern().countPatternsFrom(6, 4);
+    Future<Integer> count7  = new ScreenLockinPattern().countPatternsFrom(7, 4);
+    Future<Integer> count8  = new ScreenLockinPattern().countPatternsFrom(8, 4);
+    Future<Integer> count9  = new ScreenLockinPattern().countPatternsFrom(9, 4);
+    Integer result = count1.get(10, TimeUnit.SECONDS)+count2.get(10, TimeUnit.SECONDS)
+    +count3.get(10, TimeUnit.SECONDS)+count4.get(10, TimeUnit.SECONDS)+count5.get(10, TimeUnit.SECONDS)
+    +count6.get(10, TimeUnit.SECONDS)+count7.get(10, TimeUnit.SECONDS)+count8.get(10, TimeUnit.SECONDS)+count9.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 1624);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestLength3()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count1  = new ScreenLockinPattern().countPatternsFrom(1, 3);
+    Future<Integer> count2 = new ScreenLockinPattern().countPatternsFrom(2, 3);
+    Future<Integer> count3  = new ScreenLockinPattern().countPatternsFrom(3, 3);
+    Future<Integer> count4  = new ScreenLockinPattern().countPatternsFrom(4, 3);
+    Future<Integer> count5  = new ScreenLockinPattern().countPatternsFrom(5, 3);
+    Future<Integer> count6  = new ScreenLockinPattern().countPatternsFrom(6, 3);
+    Future<Integer> count7  = new ScreenLockinPattern().countPatternsFrom(7, 3);
+    Future<Integer> count8  = new ScreenLockinPattern().countPatternsFrom(8, 3);
+    Future<Integer> count9  = new ScreenLockinPattern().countPatternsFrom(9, 3);
+    Integer result = count1.get(10, TimeUnit.SECONDS)+count2.get(10, TimeUnit.SECONDS)
+    +count3.get(10, TimeUnit.SECONDS)+count4.get(10, TimeUnit.SECONDS)+count5.get(10, TimeUnit.SECONDS)
+    +count6.get(10, TimeUnit.SECONDS)+count7.get(10, TimeUnit.SECONDS)+count8.get(10, TimeUnit.SECONDS)+count9.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 320);
+  }
+
+  //Length 2
+  @Test
+  public void ScreenLockinPatternTestFirst9Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(9, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 5);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst8Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(8, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 7);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst7Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(7, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 5);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst6Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(6, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 7);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst4Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(4, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 7);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst5Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(5, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 8);
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst1Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(3, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 5);
+  }
 
   @Test
   public void ScreenLockinPatternTestFirst3Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
